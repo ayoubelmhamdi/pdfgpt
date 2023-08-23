c42ba2e5090b426e6d68479698a1159dc543dc36
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-08-23 05:52:36 +0100

    (install.sh) print

--- install.sh
+++ install.sh
@@ -7,3 +7,10 @@ sudo xbps-install -S \
   # tesseract-ocr-script-Arabic maybe not needed
 
 echo pip install -r requirements.txt
+
+echo pip install /content/pptgpt-0.1.6-py3-none-any.whl -qq --ignore-installed blinker
+
+echo sudo DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:alex-p/tesseract-ocr5
+echo sudo DEBIAN_FRONTEND=noninteractive apt update -y
+echo sudo DEBIAN_FRONTEND=noninteractive apt install -y poppler-utils tesseract-ocr tesseract-ocr-eng tesseract-ocr-ara tesseract-ocr-fra
+echo python3 /content/pdfgpt/pdf_to_paraphrase.py /content/2.pdf --ocr /usr/bin/tesseract 1\>res.md
