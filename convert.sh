0259dc25e32f2f80171ce833c3d55b5a0ef13a60
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-09-26 12:19:13 +0100

    (convert.sh) up05

--- convert.sh
+++ convert.sh
@@ -3,7 +3,7 @@
 CONVERSION=true
 
 if ! python3 llmtask.py                \
-    --ocr /usr/bin/tesseract-ocr       \
+    --ocr /usr/bin/tesseract           \
     --lang fr                          \
     --llm correct_ocr  \
     --file 1.pdf \
@@ -13,5 +13,5 @@ then
     echo "some errors"
 fi
 
-echo "Ayoub04"
+echo "Ayoub05"
 echo "CONVERSION=$CONVERSION" >> "$GITHUB_ENV"
