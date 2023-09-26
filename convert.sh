8cd9e4478a3a333110ce2a50168b4c9346c7a136
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-09-26 12:59:18 +0100

    (convert.sh) up 09

--- convert.sh
+++ convert.sh
@@ -4,15 +4,14 @@ CONVERSION=true
 
 if ! python3 llmtask.py                \
     --ocr /usr/bin/tesseract           \
-    --lang fr                          \
+    --lang en                          \
     --llm correct_ocr  \
     --file 1.pdf \
-    1>./src/1-2023-09-26-12:30.md
+    1>./src/1-2023-09-26-12:58.md
 then
     CONVERSION=false
     echo "some errors"
     exit 1
 fi
 
-echo "Ayoub08"
 echo "CONVERSION=$CONVERSION" >> "$GITHUB_ENV"
