0af102b6e9f5af9c8b393095e97b1b492076a8f2
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-09-26 12:31:26 +0100

    (convert.sh) push

--- convert.sh
+++ convert.sh
@@ -7,12 +7,12 @@ if ! python3 llmtask.py                \
     --lang fr                          \
     --llm correct_ocr  \
     --file 1.pdf \
-    1>./src/1.md
+    1>./src/1-2023-09-26-12:30.md
 then
     CONVERSION=false
     echo "some errors"
     exit 1
 fi
 
-echo "Ayoub07"
+echo "Ayoub08"
 echo "CONVERSION=$CONVERSION" >> "$GITHUB_ENV"
