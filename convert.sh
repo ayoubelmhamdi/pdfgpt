9cda01f142905764eb2c5c1e2c9918de59436f78
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-09-26 11:37:51 +0100

    (convert.sh) generate

---
+++ convert.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CONVERSION=true
+
+if ! python3 llmtask.py                \
+    --ocr /usr/bin/tesseract           \
+    --lang fr                          \
+    --llm correct_ocr  \
+    --file 1.pdf \
+    1>./src/1.md
+then
+    CONVERSION=false
+    echo "some errors"
+fi
+
+echo "CONVERSION=$CONVERSION" >> "$GITHUB_ENV"
