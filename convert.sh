06881d54cd0503fecb32d852e64577bd2dac8918
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-09-28 00:00:49 +0100

    update ./convert.sh in: 2023-09-28-00_00

--- convert.sh
+++ convert.sh
@@ -5,25 +5,25 @@ set -x
 # initialise the TMPDIR with tmp if not initalise yet.
 : "${TMPDIR:=/tmp}"
 
-tmp_name="$TMPDIR/new.pdf"
+tmp_name="$TMPDIR/ayoub.pdf"
 rm -f "$tmp_name"
 
-if ! wget -q --output-document "$tmp_name" "https://bashupload.com/xqub0/new.pdf";then
+if ! wget -q --output-document "$tmp_name" "https://bashupload.com/1jBCV/ayoub.pdf";then
   echo "Can not use wget to download this file."
   exit 1
 fi
 
 clear
 
-if ! echo python3 llmtask.py                \
+if ! python3 llmtask.py                \
     --ocr /usr/bin/tesseract           \
-    --lang fr                          \
+    --lang en                          \
     --llm correct_ocr  \
     --file "$tmp_name"                 \
-    1>./src/new.md
+    1>./src/ayoub.md
 then
     echo "some errors"
     exit 1
 fi
 
-echo '- [new](./new.md)' >> ./src/SUMMARY.md
+echo '- [ayoub](./ayoub.md)' >> ./src/SUMMARY.md
