92610a800205743bbe798b2215f6bc775eefb081
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-09-27 10:19:05 +0100

    up 28

--- convert.sh
+++ convert.sh
@@ -4,12 +4,12 @@
 if ! python3 llmtask.py                \
     --ocr /usr/bin/tesseract           \
     --lang en                          \
-    --llm paraphrasing  \
+    --llm correct_ocr  \
     --file 1.pdf \
-    1>./src/1-2023-09-27-10_03.md
+    1>./src/1-2023-09-27-10_17.md
 then
     echo "some errors"
     exit 1
 fi
 
-echo '- [1](./src/1-2023-09-27-10_03.md)' >> ./src/SUMMARY.md
+echo '- [1](./1-2023-09-27-10_17.md)' >> ./src/SUMMARY.md
--- src/SUMMARY.md
+++ src/SUMMARY.md
@@ -1,6 +1,4 @@
 # Summary
-Simplification using LLM 27
+Simplification using LLM 28
 
 - [INTRODUCTION](./README.md)
-- [1](./src/1-2023-09-27-09_38.md)
-- [1](./src/1-2023-09-27-10_03.md)
