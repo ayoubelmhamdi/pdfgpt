701abd182e1b8de2a5199af1f86abee4ae30e3b1
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-09-28 11:43:14 +0100

    Process ProcessNorthwestern_Thesis_Proposal_Template__1_43: 2023-09-28-11_43

--- convert.sh
+++ convert.sh
@@ -5,10 +5,10 @@ set -x
 # initialise the TMPDIR with tmp if not initalise yet.
 : "${TMPDIR:=/tmp}"
 
-tmp_name="$TMPDIR/Northwestern_Thesis_Proposal_Template__1_ (43).pdf"
+tmp_name="$TMPDIR/ProcessNorthwestern_Thesis_Proposal_Template__1_43.pdf"
 rm -f "$tmp_name"
 
-if ! wget -q --output-document "$tmp_name" "https://bashupload.com/322Wr/QKQkv.pdf";then
+if ! wget -q --output-document "$tmp_name" "https://bashupload.com/1muN_/ULiPg.pdf";then
   echo "Can not use wget to download this file."
   exit 1
 fi
@@ -18,12 +18,12 @@ clear
 if ! python3 llmtask.py                \
     --ocr /usr/bin/tesseract           \
     --lang fr                          \
-    --llm correct_ocr --llm paraphrasing  \
+    --llm correct_ocr  \
     --file "$tmp_name"                 \
-    1> './src/Northwestern_Thesis_Proposal_Template__1_ (43).md'
+    1> './src/ProcessNorthwestern_Thesis_Proposal_Template__1_43.md'
 then
     echo "some errors"
     exit 1
 fi
 
-echo '- [Northwestern_Thesis_Proposal_Template__1_ (43)](./Northwestern_Thesis_Proposal_Template__1_ (43).md)' >> ./src/SUMMARY.md
+echo '- [ProcessNorthwestern_Thesis_Proposal_Template__1_43](./ProcessNorthwestern_Thesis_Proposal_Template__1_43.md)' >> ./src/SUMMARY.md
