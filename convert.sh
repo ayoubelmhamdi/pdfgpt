5e9a45eeb392445dee42f973fac3f58bb5c8d229
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-09-29 19:33:27 +0100

    Process new_d: 2023-09-29-19_33

--- convert.sh
+++ convert.sh
@@ -5,10 +5,10 @@ set -x
 # initialise the TMPDIR with tmp if not initalise yet.
 : "${TMPDIR:=/tmp}"
 
-tmp_name="$TMPDIR/new_c.pdf"
+tmp_name="$TMPDIR/new_d.pdf"
 rm -f "$tmp_name"
 
-if ! wget -q --output-document "$tmp_name" "https://bashupload.com/E24KT/new_c.pdf";then
+if ! wget -q --output-document "$tmp_name" "https://bashupload.com/I3WN3/new_d.pdf";then
   echo "Can not use wget to download this file."
   exit 1
 fi
@@ -18,13 +18,13 @@ clear
 if ! python3 llmtask.py                \
     --ocr /usr/bin/tesseract           \
     --lang fr                          \
-    --llm correct_ocr --llm paraphrasing  \
+    --llm correct_ocr  \
     --file "$tmp_name"                 \
     --provider Aivvm                   \
-    1> './src/new_c.md'
+    1> './src/new_d.md'
 then
     echo "some errors" >&2
     exit 1
 fi
 
-echo '- [new_c](./new_c.md)' >> ./src/SUMMARY.md
+echo '- [new_d](./new_d.md)' >> ./src/SUMMARY.md
