fb0817a64f94fece30d0a7244559476c1027bf8e
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-09-30 01:04:47 +0100

    Process new_k: 2023-09-30-01_04

--- convert.sh
+++ convert.sh
@@ -5,10 +5,10 @@ set -x
 # initialise the TMPDIR with tmp if not initalise yet.
 : "${TMPDIR:=/tmp}"
 
-tmp_name="$TMPDIR/Northwestern_Thesis_Proposal_Template__1_43.pdf"
+tmp_name="$TMPDIR/new_k.pdf"
 rm -f "$tmp_name"
 
-if ! wget -q --output-document "$tmp_name" "https://bashupload.com/5_ND3/I3uXK.pdf";then
+if ! wget -q --output-document "$tmp_name" "https://bashupload.com/-NLrR/new_k.pdf";then
   echo "Can not use wget to download this file."
   exit 1
 fi
@@ -18,13 +18,13 @@ clear
 if ! python3 llmtask.py                \
     --ocr /usr/bin/tesseract           \
     --lang fr                          \
-    --llm correct_ocr --llm paraphrasing  \
+    --llm correct_ocr  \
     --file "$tmp_name"                 \
     --provider Aivvm                   \
-    1> './src/Northwestern_Thesis_Proposal_Template__1_43.md'
+    1> './src/new_k.md'
 then
     echo "some errors" >&2
     exit 1
 fi
 
-echo '- [Northwestern_Thesis_Proposal_Template__1_43](./Northwestern_Thesis_Proposal_Template__1_43.md)' >> ./src/SUMMARY.md
+echo '- [new_k](./new_k.md)' >> ./src/SUMMARY.md
