72390cb4545b683f401c79f1e8a57accca496d96
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-09-30 13:53:48 +0100

    Process a2: 2023-09-30-13_53

--- convert.sh
+++ convert.sh
@@ -8,7 +8,7 @@ set -x
 tmp_name="$TMPDIR/a2.pdf"
 rm -f "$tmp_name"
 
-if ! wget -q --output-document "$tmp_name" "https://bashupload.com/shefM/a2.pdf";then
+if ! wget -q --output-document "$tmp_name" "https://bashupload.com/EEdhn/a2.pdf";then
   echo "Can not use wget to download this file."
   exit 1
 fi
@@ -20,8 +20,11 @@ if ! python3 llmtask.py                \
     --lang en                          \
     --llm correct_ocr  \
     --file "$tmp_name"                 \
-    --provider Aivvm
+    --provider Aivvm                   \
+    1> './src/a2-2023-09-30-13_53.md'
 then
     echo "some errors" >&2
     exit 1
 fi
+
+echo '- [a2](./a2-2023-09-30-13_53.md)' >> ./src/SUMMARY.md
