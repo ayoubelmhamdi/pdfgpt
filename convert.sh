c3297df2b62368c72f4e1b008c2c4d266c8054c8
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-09-30 02:33:51 +0100

    Process a2: 2023-09-30-02_33

--- convert.sh
+++ convert.sh
@@ -5,10 +5,10 @@ set -x
 # initialise the TMPDIR with tmp if not initalise yet.
 : "${TMPDIR:=/tmp}"
 
-tmp_name="$TMPDIR/Northwestern_Thesis_Proposal_Template__1_43.pdf"
+tmp_name="$TMPDIR/a2.pdf"
 rm -f "$tmp_name"
 
-if ! wget -q --output-document "$tmp_name" "https://bashupload.com/sjwvt/v5lnk.pdf";then
+if ! wget -q --output-document "$tmp_name" "https://bashupload.com/shefM/a2.pdf";then
   echo "Can not use wget to download this file."
   exit 1
 fi
@@ -17,14 +17,14 @@ clear
 
 if ! python3 llmtask.py                \
     --ocr /usr/bin/tesseract           \
-    --lang fr                          \
-    --llm correct_ocr --llm paraphrasing  \
+    --lang en                          \
+    --llm correct_ocr  \
     --file "$tmp_name"                 \
     --provider Aivvm                   \
-    1> './src/Northwestern_Thesis_Proposal_Template__1_43.md'
+    1> './src/a2.md'
 then
     echo "some errors" >&2
     exit 1
 fi
 
-echo '- [Northwestern_Thesis_Proposal_Template__1_43](./Northwestern_Thesis_Proposal_Template__1_43.md)' >> ./src/SUMMARY.md
+echo '- [a2](./a2.md)' >> ./src/SUMMARY.md
