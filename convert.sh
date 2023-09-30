34b2276571616460c7c981c2d736e25286acfebb
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-10-01 00:37:02 +0100

    (convert.sh) include aivvm

--- convert.sh
+++ convert.sh
@@ -3,15 +3,5 @@
 set -x
 
 
-# if ! python3 llmtask.py                \
-#     --lang fr                          \
-#     --llm correct_ocr                  \
-#     --file "/tmp/1.txt"                \
-#     --provider Aivvm
-# then
-#     echo "some errors" >&2
-#     exit 1
-# fi
-
 "${0%/*}/aiivm2.py"
 
