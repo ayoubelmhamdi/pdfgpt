c0802d6b7f5073bc137daa8079068174f6546257
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-10-01 00:35:37 +0100

    (convert.sh) try new headers

--- convert.sh
+++ convert.sh
@@ -2,29 +2,16 @@
 
 set -x
 
-# initialise the TMPDIR with tmp if not initalise yet.
-: "${TMPDIR:=/tmp}"
 
-tmp_name="$TMPDIR/Northwestern_Thesis_Proposal_Template__1_43.pdf"
-rm -f "$tmp_name"
+# if ! python3 llmtask.py                \
+#     --lang fr                          \
+#     --llm correct_ocr                  \
+#     --file "/tmp/1.txt"                \
+#     --provider Aivvm
+# then
+#     echo "some errors" >&2
+#     exit 1
+# fi
 
-if ! wget -q --output-document "$tmp_name" "https://bashupload.com/m9QNC/Ab8Rq.pdf";then
-  echo "Can not use wget to download this file."
-  exit 1
-fi
+"${0%/*}/aiivm2.py"
 
-clear
-
-if ! python3 llmtask.py                \
-    --ocr /usr/bin/tesseract           \
-    --lang fr                          \
-    --llm correct_ocr --llm paraphrasing  \
-    --file "$tmp_name"                 \
-    --provider Aivvm                   \
-    1> './src/Northwestern_Thesis_Proposal_Template__1_43.md'
-then
-    echo "some errors" >&2
-    exit 1
-fi
-
-echo '- [Northwestern_Thesis_Proposal_Template__1_43](./Northwestern_Thesis_Proposal_Template__1_43.md)' >> ./src/SUMMARY.md
