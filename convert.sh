a93595d9351b91a7ce95951504309d38469c63ae
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-09-30 16:34:51 +0100

    Process 1: 2023-09-30-16_34

--- convert.sh
+++ convert.sh
@@ -5,27 +5,26 @@ set -x
 # initialise the TMPDIR with tmp if not initalise yet.
 : "${TMPDIR:=/tmp}"
 
-tmp_name="$TMPDIR/a2.pdf"
+tmp_name="$TMPDIR/1.pdf"
 rm -f "$tmp_name"
 
-if ! wget -q --output-document "$tmp_name" "https://bashupload.com/EEdhn/a2.pdf";then
-  echo "Can not use wget to download this file."
+if ! wget -q --output-document "$tmp_name" "https://bashupload.com/kHQ1n/1.pdf";then
+  echo "Can not use wget to download this file." >&2
   exit 1
 fi
-
 
 clear
 
 if ! python3 llmtask.py                \
     --ocr /usr/bin/tesseract           \
-    --lang en                          \
+    --lang fr                          \
     --llm correct_ocr  \
     --file "$tmp_name"                 \
     --provider Aivvm                   \
-    1> './src/a2-2023-09-30-13_53.md'
+    1> './src/1.md'
 then
     echo "some errors" >&2
     exit 1
 fi
 
-echo '- [a2](./a2-2023-09-30-13_53.md)' >> ./src/SUMMARY.md
+echo '- [1](./1.md)' >> ./src/SUMMARY.md
