d70d466adcd20e20cb4701f8b09de70763abc47c
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-10-01 02:45:05 +0100

    (aiivm2.py) use ani scrap

--- aiivm2.py
+++ aiivm2.py
@@ -4,20 +4,16 @@ import cloudscraper
 
 scraper = cloudscraper.create_scraper()  # returns a CloudScraper instance
 
-user="calcauer 2+98"
-admin="You are chatgpt"
-
-json_data = {
-    "model": {'id': 'gpt-4', 'name': 'GPT-4'},
-    "messages": [{"role": "user", "content": user}],
-    "key": "",
-    "prompt": admin,
-    "temperature": 0.7,
-}
-
-response = scraper.post(
-    "https://chat.aivvm.com/api/chat",
-    # headers=headers,
-    json=json_data,
-)
-print(response.text)
+def ask_gpt4(user=None, admin=None):
+    json_data = {
+        "model": {'id': 'gpt-4', 'name': 'GPT-4'},
+        "messages": [{"role": "user", "content": user}],
+        "key": "",
+        "prompt": admin,
+        "temperature": 0.7,
+    }
+    
+    response = scraper.post(
+        "https://chat.aivvm.com/api/chat",
+        json=json_data,
+    )
