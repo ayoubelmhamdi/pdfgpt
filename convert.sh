97b9aae3f9d397c814ab3901889155d2c4a7b49e
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-10-02 22:09:06 +0100

    try this

--- convert.sh
+++ convert.sh
@@ -5,15 +5,15 @@ set -x
 # initialise the TMPDIR with tmp if not initalise yet.
 : "${TMPDIR:=/tmp}"
 
-tmp_name="$TMPDIR/1.md"
-rm -f "$tmp_name"
-
-if ! wget -q --output-document "$tmp_name" "https://bashupload.com/l08VD/1.md";then
-  echo "Can not use wget to download this file." >&2
-  exit 1
-fi
-
-clear
+tmp_name="1.md"
+# rm -f "$tmp_name"
+#
+# if ! wget -q --output-document "$tmp_name" "https://bashupload.com/l08VD/1.md";then
+#   echo "Can not use wget to download this file." >&2
+#   exit 1
+# fi
+#
+# clear
 
 if ! python3 llmtask.py                \
     --ocr /usr/bin/tesseract           \
