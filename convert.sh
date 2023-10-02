d00b1e6ea1a78b58a287cee5f0f7cf68b30ec767
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-10-02 11:27:50 +0100

    Process 1: 2023-10-02-11_27

--- convert.sh
+++ convert.sh
@@ -8,18 +8,17 @@ set -x
 tmp_name="$TMPDIR/1.md"
 rm -f "$tmp_name"
 
-if ! wget -q --output-document "$tmp_name" "https://bashupload.com/rtr8w/1.md";then
+if ! wget -q --output-document "$tmp_name" "https://bashupload.com/abHrb/1.md";then
   echo "Can not use wget to download this file." >&2
   exit 1
 fi
-
 
 clear
 
 if ! python3 llmtask.py                \
     --ocr /usr/bin/tesseract           \
     --lang fr                          \
-    --llm correct_ocr --llm paraphrasing  \
+    --llm correct_ocr  \
     --file "$tmp_name"                 \
     --provider Aivvm                   \
     1> './src/1.md'
