e53bf801433a43aad532d38c3e1552cbcd2ac4a5
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-10-04 21:18:59 +0100

    add nevim

--- console.sh
+++ console.sh
@@ -14,6 +14,8 @@ sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent softwar
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
+sudo apt install neovim
+sudo ln -s "$(which nvim)" /usr/bin/v
 sudo apt -y install docker-ce docker-ce-cli containerd.io
 
 # curl --proto '=https' --tlsv1.2 -sSf  https://raw.githubusercontent.com/ayoubelmhamdi/pdfgpt/master/console.sh | bash
