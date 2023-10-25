fbc717105e839e7a3e52dbb9eee06951d86f3c1f
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-10-25 19:42:43 +0100

    (convert.sh) up2

--- convert.sh
+++ convert.sh
@@ -2,20 +2,9 @@
 
 set -x
 
-# initialise the TMPDIR with tmp if not initalise yet.
 : "${TMPDIR:=/tmp}"
 
 tmp_name="1.md"
-# rm -f "$tmp_name"
-#
-# if ! wget -q --output-document "$tmp_name" "https://bashupload.com/l08VD/1.md";then
-#   echo "Can not use wget to download this file." >&2
-#   exit 1
-# fi
-#
-# clear
-
-
 
 if ! python3 llmtask.py                \
     --ocr /usr/bin/tesseract           \
@@ -23,10 +12,10 @@ if ! python3 llmtask.py                \
     --llm correct_ocr  \
     --file "$tmp_name"                 \
     --provider Chatgptbz                   \
-    1> './src/1-e2.md'
+    1> './src/1-e3.md'
 then
     echo "some errors" >&2
     exit 1
 fi
 
-echo '- [1](./1-e2.md)' >> ./src/SUMMARY.md
+echo '- [1](./1-e3.md)' >> ./src/SUMMARY.md
