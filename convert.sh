a53549bcbe88a9c323ec9353e7586f070dc135e5
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-10-26 11:47:37 +0100

    Process Northwestern_Thesis_Proposal_Template__1_43: 2023-10-26-11_47

--- convert.sh
+++ convert.sh
@@ -5,11 +5,11 @@ set -x
 # initialise the TMPDIR with tmp if not initalise yet.
 : "${TMPDIR:=/tmp}"
 
-tmp_name="$TMPDIR/2.pdf"
-name="2-11h00"
+tmp_name="$TMPDIR/Northwestern_Thesis_Proposal_Template__1_43.pdf"
+name="Northwestern_Thesis_Proposal_Template__1_43"
 rm -f "$tmp_name"
 
-if ! wget -q --output-document "$tmp_name" "https://bashupload.com/oFQq4/2.pdf";then
+if ! wget -q --output-document "$tmp_name" "https://bashupload.com/cVygn/ycf4F.pdf";then
   echo "Can not use wget to download this file." >&2
   exit 1
 fi
@@ -19,7 +19,7 @@ clear
 if ! python3 llmtask.py                \
     --ocr /usr/bin/tesseract           \
     --lang fr                          \
-    --llm correct_ocr  \
+    --llm correct_ocr --llm paraphrasing  \
     --file "$tmp_name"                 \
     --provider Chatgptbz               \
     1> "./src/${name}.md"
