c999bb348369af4212925934ebd9724e6df63c9d
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-10-26 04:50:23 +0100

    Process 2: 2023-10-26-04_50

--- convert.sh
+++ convert.sh
@@ -2,20 +2,30 @@
 
 set -x
 
+# initialise the TMPDIR with tmp if not initalise yet.
 : "${TMPDIR:=/tmp}"
 
-tmp_name="1.md"
+tmp_name="$TMPDIR/2.pdf"
+rm -f "$tmp_name"
+
+if ! wget -q --output-document "$tmp_name" "https://bashupload.com/K3aXD/2.pdf";then
+  echo "Can not use wget to download this file." >&2
+  exit 1
+fi
+
+clear
 
 if ! python3 llmtask.py                \
     --ocr /usr/bin/tesseract           \
     --lang fr                          \
     --llm correct_ocr  \
     --file "$tmp_name"                 \
-    --provider Chatgptbz                   \
-    1> './src/1-e8.md'
+    --provider Aivvm                   \
+    1> './src/2.md'
 then
     echo "some errors" >&2
     exit 1
 fi
 
-echo '- [8](./1-e8.md)' >> ./src/SUMMARY.md
+sed -i -e 's#\\\[\(\([^\]*\)[^]]\+\)\\]#$$\1$$#g' -e 's#\\(\(\([^\]*\)[^]]\+\))#$\1$#g' './src/2.md'
+echo '- [2](./2.md)' >> ./src/SUMMARY.md
