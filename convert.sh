7bed82fe6c828cdf36066806b9ffd76d38f6cfcf
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-10-26 16:16:39 +0100

    try 1.md

--- convert.sh
+++ convert.sh
@@ -2,22 +2,11 @@
 
 set -x
 
-# initialise the TMPDIR with tmp if not initalise yet.
-: "${TMPDIR:=/tmp}"
-
-tmp_name="$TMPDIR/Northwestern_Thesis_Proposal_Template__1_43.pdf"
-name="Northwestern_Thesis_Proposal_Template__1_43"
-rm -f "$tmp_name"
-
-if ! wget -q --output-document "$tmp_name" "https://bashupload.com/_CVDL/kUPAS.pdf";then
-  echo "Can not use wget to download this file." >&2
-  exit 1
-fi
-
-clear
+tmp_name="1.md"
+name="004"
 
 if ! python3 llmtask.py                \
-    --ocr /usr/bin/tesseract           \
+    --ocr /usr/bin/tesseract-ocr           \
     --lang fr                          \
     --llm correct_ocr --llm paraphrasing  \
     --file "$tmp_name"                 \
@@ -30,8 +19,8 @@ fi
 
 echo "- [${name}](${name}.md)" >> ./src/SUMMARY.md
 left="$(grep -o -e '\\(\(\([^\]*\)[^)]\+\))' -e '\\\[\(\([^\]*\)[^]]\+\)]' "./src/${name}.md" | wc -l)"
-echo "left=$left"
 dollar="$(grep -o '\$\+\([^$]\+\)\$\+' "./src/${name}.md" | wc -l)"
+echo "left=$left"
 echo "dollar=$dollar"
 
 if [ "$dollar" -gt "$left" ];then
