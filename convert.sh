e0eab7b83b2929c0aa4102b3dff6a9f70b77dd76
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-10-26 06:36:40 +0100

    Process 2: 2023-10-26-06_36

--- convert.sh
+++ convert.sh
@@ -5,13 +5,11 @@ set -x
 # initialise the TMPDIR with tmp if not initalise yet.
 : "${TMPDIR:=/tmp}"
 
-
-
 tmp_name="$TMPDIR/2.pdf"
-name="2-a"
+name="2"
 rm -f "$tmp_name"
 
-if ! wget -q --output-document "$tmp_name" "https://bashupload.com/0DL1Z/2.pdf";then
+if ! wget -q --output-document "$tmp_name" "https://bashupload.com/byc1U/2.pdf";then
   echo "Can not use wget to download this file." >&2
   exit 1
 fi
