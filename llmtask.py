9f56b74fa6e7fe29679db2aaa0c427f9d7c1c21e
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-10-28 17:52:21 +0100

    (llmtask.py) use log to stderr

--- llmtask.py
+++ llmtask.py
@@ -11,15 +11,17 @@ from pptgpt import CreateParaphrasing
 
 load_dotenv(".env", verbose=True, override=True)
 
+def log(*args):
+    print(*args, file=sys.stderr)
 
 def read_txt_file(file_path):
     try:
         with open(file_path, "r") as file:
             return file.read()
     except FileNotFoundError as e:
-        print(f"Error: The file '{file_path}' was not found: {e}")
+        log(f"Error: The file '{file_path}' was not found: {e}")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        log(f"An error occurred: {e}")
     return None
 
 
@@ -34,29 +36,29 @@ def detect_file_type(file_path):
         else:
             return None
     except magic.MagicException as e:
-        print(f"Error: {e}")
+        log(f"Error: {e}")
         sys.exit(1)
 
 
 def pdf_to_images(pdf=None):
     if pdf is None:
-        print("No file pdf provid", file=sys.stderr)
+        log("No file pdf provid")
         sys.exit(1)
     return convert_from_path(pdf)
 
 
 def images_to_garbage_texts(images=None):
     if images is None:
-        print("E006: can not extract images from pdf", file=sys.stderr)
+        log("E006: can not extract images from pdf")
         sys.exit(1)
     texts = []
     num_images = len(images)
     for i in range(num_images):
         try:
             texts.append(pytesseract.image_to_string(images[i]))
-            print(f"ocr image {i+1}/{num_images} ✔", file=sys.stderr)
+            log(f"ocr image {i+1}/{num_images} ✔")
         except KeyError as e:
-            print(f"ocr not found or because of {e}", file=sys.stderr)
+            log(f"ocr not found or because of {e}")
 
     if len(texts) == 0:
         sys.exit(1)
@@ -86,11 +88,11 @@ if __name__ == "__main__":
     args = parser.parse_args(sys.argv[1:])
 
     if args.file is None:
-        print("Please provide a Pdf file.", file=sys.stderr)
+        log("Please provide a Pdf file.")
         sys.exit(2)
 
     if not os.path.exists(args.file):
-        print(f"File {args.file} does not exist.", file=sys.stderr)
+        log(f"File {args.file} does not exist.")
         sys.exit(1)
 
     #################################################
@@ -101,7 +103,7 @@ if __name__ == "__main__":
         lang = "en"
 
     if not args.llm or len(args.llm) == 0:
-        print("Please provide at least one task for LLM.", file=sys.stderr)
+        log("Please provide at least one task for LLM.")
         sys.exit(3)
 
     if not is_valid_subset(args.llm):
@@ -113,27 +115,27 @@ if __name__ == "__main__":
 
     if file_type == "pdf":
         if args.ocr is None:
-            print("Please provide an OCR FILEPATH.", file=sys.stderr)
+            log("Please provide an OCR FILEPATH.")
             sys.exit(2)
         if not os.path.exists(args.ocr):
-            print(f"File {args.ocr} does not exist.", file=sys.stderr)
+            log(f"File {args.ocr} does not exist.")
             sys.exit(1)
 
 
         pytesseract.pytesseract.tesseract_cmd = args.ocr
         images = pdf_to_images(args.file)
         garbage_texts = images_to_garbage_texts(images)
-        print("images ✔", file=sys.stderr)
+        log("images ✔")
     elif file_type == "txt":
         garbage_texts = read_txt_file(args.file)
-        print("Texts ✔", file=sys.stderr)
+        log("Texts ✔")
     else:
-        print("The file type is not recognized as PDF or TXT.", file=sys.stderr)
+        log("The file type is not recognized as PDF or TXT.")
         sys.exit(1)
 
     #################################################
     if garbage_texts is None:
-        print("garbage_texts is None", file=sys.stderr)
+        log("garbage_texts is None")
         sys.exit(1)
 
     lists = CreateParaphrasing(
